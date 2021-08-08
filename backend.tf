6e4901e3f455fb88b9328eec3ad4d597b64a95cc
Chidiebere Ugwu <ugwuchidieberej@gmail.com>
2021-08-08 09:20:10 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "qa-cluster"
     key    = "terraform/state/db"
-    access_key = "AKIA6AXTYJEUCYZBZTOV"
-    secret_key = "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"
+    access_key = "AKxxxxxxxxxxxxxxxxxxOV"
+    secret_key = "KFxxxxxxxxxxxxxxtrr2h"
   }
 }
